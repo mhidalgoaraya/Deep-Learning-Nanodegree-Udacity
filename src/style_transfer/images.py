f1feda4744bb2741b4110605e3ecd5b3205517ac
Marco Hidalgo <marhiar@gmail.com>
2020-04-25 18:53:35 +0200

    scripts for style transfer exercise

---
+++ src/style_transfer/images.py
@@ -0,0 +1,104 @@
+import numpy as np
+import torch
+from PIL import Image
+from torchvision import transforms, models
+
+
+def from_image_to_tensor(img_path, max_size=400, shape=None):
+    """ Load in and transform an image, making sure the image
+       is <= 400 pixels in the x-y dims.
+
+    :param img_path: str. Path were image is located
+    :param max_size: default 400
+    :param shape:
+    :return image: transformed image into tensor
+    """
+
+    image = Image.open(img_path).convert('RGB')
+
+    # large images will slow down processing
+    if max(image.size) > max_size:
+        size = max_size
+    else:
+        size = max(image.size)
+
+    if shape is not None:
+        size = shape
+
+    in_transform = transforms.Compose([
+        transforms.Resize(size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406),
+                             (0.229, 0.224, 0.225))])
+
+    # discard the transparent, alpha channel (that's the :3) and add the batch dimension
+    image = in_transform(image)[:3, :, :].unsqueeze(0)
+
+    return image
+
+
+def im_convert(tensor):
+    """  Display a tensor as an image. Un-normalizing an image
+    and converting it from a Tensor image to a NumPy image for display
+
+    :param tensor: Image tensor
+    :return image: Converted image into numpy.
+    """
+
+    image = tensor.to("cpu").clone().detach()
+    image = image.numpy().squeeze()
+    image = image.transpose(1, 2, 0)
+    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
+    image = image.clip(0, 1)
+
+    return image
+
+
+def get_content_style_features(image, model, layers=None):
+    """ Run an image forward through a model and get the features for
+        a set of layers. Default layers are for VGGNet matching Gatys et al (2016)
+
+    :param image:
+    :param model:
+    :param layers:
+    :return features:
+    """
+
+    # Mapping layer names of PyTorch's VGGNet to names from the paper
+    # Layers from the content and style representations of an image
+    if layers is None:
+        layers = {'0': 'conv1_1',
+                  '5': 'conv2_1',
+                  '10': 'conv3_1',
+                  '19': 'conv4_1',
+                  '21': 'conv4_2',
+                  '28': 'conv5_1'}
+
+    features = {}
+    x = image
+    # model._modules is a dictionary holding each module in the model
+    for name, layer in model._modules.items():
+        x = layer(x)
+        if name in layers:
+            features[layers[name]] = x
+
+    return features
+
+
+def gram_matrix(tensor):
+    """ Calculate the Gram Matrix of a given tensor
+        Gram Matrix: https://en.wikipedia.org/wiki/Gramian_matrix
+    :param tensor:
+    :return gram:
+    """
+
+    # get the batch_size, depth, height, and width of the Tensor
+    _, d, h, w = tensor.size()
+
+    # reshape so we're multiplying the features for each channel
+    tensor = tensor.view(d, h * w)
+
+    # calculate the gram matrix
+    gram = torch.mm(tensor, tensor.t())
+
+    return gram
