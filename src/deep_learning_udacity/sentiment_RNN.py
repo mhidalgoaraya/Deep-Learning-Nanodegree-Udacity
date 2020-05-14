80ae6848092a4f4cd43ecc9bcdfb83b04714ed17
Marco Hidalgo <marhiar@gmail.com>
2020-05-14 14:49:15 +0200

    Add Sentiment RNN exercise

---
+++ src/deep_learning_udacity/sentiment_RNN.py
@@ -0,0 +1,151 @@
+import numpy as np
+import torch.nn as nn
+import torch
+from string import punctuation
+
+
+def pad_features(reviews_ints, seq_length):
+    ''' Return features of review_ints, where each review is padded with 0's
+        or truncated to the input seq_length.
+    '''
+    ## implement function
+
+    features = np.zeros((len(reviews_ints), seq_length), dtype=int)
+
+    for i, row in enumerate(reviews_ints):
+        features[i, -len(row):] = np.array(row)[:seq_length]
+
+    return features
+
+
+class SentimentRNN(nn.Module):
+    """
+    The RNN model that will be used to perform Sentiment analysis.
+    """
+
+    def __init__(self, train_on_gpu, vocab_size, output_size, embedding_dim,
+                 hidden_dim, n_layers,
+                 drop_prob=0.5):
+        """
+        Initialize the model by setting up the layers.
+        """
+        super(SentimentRNN, self).__init__()
+
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        self.train_on_gpu = train_on_gpu
+
+        # embedding and LSTM layers
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers,
+                            dropout=drop_prob, batch_first=True)
+
+        # dropout layer
+        self.dropout = nn.Dropout(0.3)
+
+        # linear and sigmoid layers
+        self.fc = nn.Linear(hidden_dim, output_size)
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x, hidden):
+        """
+        Perform a forward pass of our model on some input and hidden state.
+        """
+        batch_size = x.size(0)
+
+        # embeddings and lstm_out
+        x = x.long()
+        embeds = self.embedding(x)
+        lstm_out, hidden = self.lstm(embeds, hidden)
+
+        # stack up lstm outputs
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+
+        # dropout and fully-connected layer
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        # sigmoid function
+        sig_out = self.sig(out)
+
+        # reshape to be batch_size first
+        sig_out = sig_out.view(batch_size, -1)
+        sig_out = sig_out[:, -1]  # get last batch of labels
+
+        # return last sigmoid output and hidden state
+        return sig_out, hidden
+
+    def init_hidden(self, batch_size):
+        ''' Initializes hidden state '''
+        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
+        # initialized to zero, for hidden state and cell state of LSTM
+        weight = next(self.parameters()).data
+
+        if self.train_on_gpu:
+            hidden = (
+            weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda(),
+            weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda())
+        else:
+            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+                      weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+
+        return hidden
+
+
+def tokenize_review(test_review, vocab_to_int):
+    test_review = test_review.lower() # lowercase
+    # get rid of punctuation
+    test_text = ''.join([c for c in test_review if c not in punctuation])
+
+    # splitting by spaces
+    test_words = test_text.split()
+
+    # tokens
+    test_ints = []
+    test_ints.append([vocab_to_int[word] for word in test_words])
+
+    return test_ints
+
+
+def predict(net, train_on_gpu, test_review, sequence_length=200):
+    ''' Prints out whether a give review is predicted to be
+        positive or negative in sentiment, using a trained model.
+
+        params:
+        net - A trained net
+        test_review - a review made of normal text and punctuation
+        sequence_length - the padded length of a review
+        '''
+    net.eval()
+
+    # tokenize review
+    test_ints = tokenize_review(test_review)
+
+    # pad tokenized sequence
+    seq_length = sequence_length
+    features = pad_features(test_ints, seq_length)
+
+    # convert to tensor to pass into your model
+    feature_tensor = torch.from_numpy(features)
+
+    batch_size = feature_tensor.size(0)
+
+    # initialize hidden state
+    h = net.init_hidden(batch_size)
+
+    if train_on_gpu:
+        feature_tensor = feature_tensor.cuda()
+
+    # get the output from the model
+    output, h = net(feature_tensor, h)
+
+    # convert output probabilities to predicted class (0 or 1)
+    pred = torch.round(output.squeeze())
+    # printing output value, before rounding
+    print('Prediction value, pre-rounding: {:.6f}'.format(output.item()))
+
+    # print custom response
+    if pred.item() == 1:
+        print("Positive review detected!")
+    else:
+        print("Negative review detected.")
